29ebea1739773e558e023297fa4e28d90315d9f9
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-10-14 18:51:59 +0100

    🏷️ added types for mongodb collection

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+export interface ChatRoom {
+  chatRoomId: string,
+  users: User[], // max 2
+  messages?: Message[],
+}
+
+interface User {
+  userId: string,
+  userName: string,
+}
+
+interface Message {
+  fromUserId: string,
+  content: string,
+}
