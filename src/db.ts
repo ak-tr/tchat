2e21267dbdbf7cb9d4ec96a8dda0337a64c20f77
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-10-15 19:09:42 +0100

    ✨ added user related functions to `db.ts`

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,9 @@
 import { MongoClient } from "mongodb";
 import { Db, Collection } from "mongodb/mongodb";
 import { ChatRoom } from "./types";
+import { User } from "./user";
+import { UserInDatabase } from "./types"
+import crypto from "crypto";
 
 // Initialise dotenv for environment variables
 import * as dotenv from "dotenv";
@@ -9,19 +12,20 @@ dotenv.config();
 let client: MongoClient;
 let database: Db;
 let chatRooms: Collection<ChatRoom>;
+let users: Collection<UserInDatabase>
 
 export const connectToDatabase = () => {
   client = new MongoClient(process.env.CONN_STRING);
   database = client.db("Tchat");
   
-  // Access ChatRooms collection on database
-  // Collection on MongoDB acts as server
-  chatRooms = database.collection<ChatRoom>("ChatRooms")
+  chatRooms = database.collection<ChatRoom>("ChatRooms");
+  users = database.collection<UserInDatabase>("Users");
 }
 
-export const createChatRoom = async (userId: string, userName: string) => {
+export const createChatRoom = async (user: User) => {
   // Generate random chat room ID e.g. 3VQAPMYG
   const chatRoomId = generateChatRoomId();
+  const { userId, userName } = user.getUserDetails();
 
   await chatRooms.insertOne({
     chatRoomId,
@@ -41,7 +45,50 @@ export const checkIfChatRoomExists = async (chatRoomId: string) => {
   return !!(await chatRooms.count({ chatRoomId }, { limit: 1 }))
 }
 
-export const sendMessage = async (chatRoomId: string, userId: string, userName: string, content: string) => {
+export const addUserToChatRoom = async (user: User, chatRoomId: string) => {
+  const result = await chatRooms.updateOne(
+    { chatRoomId },
+    { $addToSet: { users: user.getUserDetails() } }
+  )
+
+  return result.modifiedCount >= 1;
+}
+
+export const userSignUp = async (user: User, password: string) => {
+  const { userId, userName } = user.getUserDetails();
+
+  const result = await users.updateOne(
+    {
+      userName,
+    },
+    { $set: {
+        userId,
+        userName,
+        password: hashPassword(password),
+      },
+    },
+    {
+      upsert: true,
+    }
+  )
+
+  return result.upsertedCount >= 1;
+}
+
+export const userSignIn = async (userName: string, password: string) => {
+  const hashedPassword = hashPassword(password);
+
+  const result = await users.findOne({
+    userName,
+    password: hashedPassword,
+  })
+
+  return result;
+}
+
+export const sendMessage = async (chatRoomId: string, user: User, content: string) => {
+  const { userId, userName } = user.getUserDetails();
+
   const result = await chatRooms.updateOne(
     { chatRoomId },
     { $push: { "messages": { fromUserId: userId, fromUserName: userName, content, timestamp: new Date().getTime() } } }
@@ -55,5 +102,9 @@ export const closeDatabase = () => {
 }
 
 const generateChatRoomId = () => {
-  return (Math.random() + 1).toString(36).substring(5).toUpperCase();
+  return Math.random().toString(36).slice(2, 7).toUpperCase();
+}
+
+const hashPassword = (password: string) => {
+  return crypto.createHash("SHA256").update(password).digest("hex");
 }
